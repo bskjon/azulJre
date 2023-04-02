5388fdb370275fadb82f0ef6b5f50926b11e005b
Brage Skjonborg <brage.skjonborg@hotmail.no>
2023-04-02 22:52:16 +0200

    Changed to manual

--- installer.sh
+++ installer.sh
@@ -5,21 +5,12 @@ mkdir -p /usr/sbin/
 echo "Building with => " $TARGETARCH
 
 
-mkdir -p /opt/java/java17/
-if [[ $TARGETARCH == "arm"* ]] 
-then 
-    cp -vrf dep/arm/java/* /opt/java/java17/
-else
-    cp -vrf dep/amd/java/* /opt/java/java17/
-fi
-
-
-if [ -z "$(ls -A -- "/opt/java/java17")" ]; then
+if [ -z "$(ls -A -- "/opt/java/java$JRE")" ]; then
     printf "\n\nJava folder is empty!\n\n"
     printf "\nExiting now =>"
     exit 1
 fi
-chmod +x /opt/java/java17/bin/java
+chmod +x /opt/java/java$JRE/bin/java
 
 printf "\n\Setting default Java\n"
-update-alternatives --install /usr/bin/java java /opt/java/java17/bin/java 100
+update-alternatives --install /usr/bin/java java /opt/java/java$JRE/bin/java 100
