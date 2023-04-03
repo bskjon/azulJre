85e0ec0aa293535baa02a5b3cf76359a74098715
Brage Skjonborg <brage.skjonborg@hotmail.no>
2023-04-03 19:11:56 +0200

    Updated entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,6 +12,12 @@ printenv
 
 set -e
 
+if [ -z "${JAVA_ENTRYPOINT_QUIET_LOGS:-}" ]; then
+    exec 3>&1
+else
+    exec 3>/dev/null
+fi
+
 if [ "$1" = "java" ]; then
     if /usr/bin/find "/docker-entrypoint.d/" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read v; then
         echo >&3 "$0: /docker-entrypoint.d/ is not empty, will attempt to perform configuration"
